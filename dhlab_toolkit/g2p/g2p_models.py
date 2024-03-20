ec1c0b51b955648d12b65a31d255829119d4ace8
Ingerid Dale <ingerid.dale@nb.no>
2024-03-20 16:38:24 +0100

    feat: enable anonymous google download

--- dhlab_toolkit/g2p/g2p_models.py
+++ dhlab_toolkit/g2p/g2p_models.py
@@ -4,7 +4,19 @@ from google.cloud import storage
 import phonetisaurus
 import logging
 
-logging.basicConfig(level=logging.INFO)
+
+def download_public_file(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a public blob from the bucket."""
+    # bucket_name = "your-bucket-name"
+    # source_blob_name = "storage-object-name"
+    # destination_file_name = "local/path/to/file"
+
+    storage_client = storage.Client.create_anonymous_client()
+
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+
 
 
 def download_g2p_model(dialect='e', style= "written"):
@@ -15,14 +27,11 @@ def download_g2p_model(dialect='e', style= "written"):
     download_dir = Path.home() / ".cache" / "dhlab_toolkit" 
     download_dir.mkdir(parents=True, exist_ok=True)
     download_path = download_dir / filename
+    bucket_name = "g2p-models"
     
     if not download_path.exists():        
         try:
-            client = storage.Client()
-            bucket_name = "g2p-models"
-            bucket = client.get_bucket(bucket_name)
-            blob = bucket.blob(filename)
-            blob.download_to_filename(download_path)
+            download_public_file(bucket_name, filename, download_path)
             logging.debug("Download successful.")
         except Exception as e:
             logging.error(e)
@@ -42,9 +51,11 @@ def transcribe(words, dialect='e', style= "written"):
 
 
 if __name__ == "__main__":
-    #download_g2p_model(dialect='n', style= "written")
-    result = transcribe(["I", "Nasjonalbiblioteket", "har", "vi", "veldig", "mange", "gamle", "og", "sjeldne", "bøker"])
-    for word, pronunciation in result:
-        print(f"{word}\t{' '.join(pronunciation)}")
-        
+    logging.basicConfig(level=logging.DEBUG)
+
+    download_g2p_model(dialect='t', style= "written")
+    #result = transcribe(["I", "Nasjonalbiblioteket", "har", "vi", "veldig", "mange", "gamle", "og", "sjeldne", "bøker"])
+    #for word, pronunciation in result:
+     #   print(f"{word}\t{' '.join(pronunciation)}")
+    
 # %%
